056ad83695d260310fe2906a5c744ec4f76c88ec
Justin <justin.leung.ch@gmail.com>
2020-12-24 23:24:42 -0500

    Edit server.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ if (!isDev && cluster.isMaster) {
   const app = express();
 
   // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
 
   // Answer API requests.
   app.get('/api', function (req, res) {
@@ -33,7 +33,7 @@ if (!isDev && cluster.isMaster) {
 
   // All remaining requests return the React app, so it can handle routing.
   app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+    response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
 
   app.listen(PORT, function () {
