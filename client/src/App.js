48d789729935663768bf7304bc3a6d4e69beb2d4
Justin <justin.leung.ch@gmail.com>
2020-12-25 14:04:06 -0500

    Add user API

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  function getUser() {
+    fetch('/users')
+    .then(response => {
+      console.log(response.text)
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
