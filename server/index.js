b77e384aa52ff2c47de776d410f469b7294badb6
Justin <justin.leung.ch@gmail.com>
2020-12-26 17:57:14 -0500

    Successfully added user authentication routing

--- server/index.js
+++ server/index.js
@@ -17,11 +17,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
   app.use(express.static('client/build'));
-  
-const path = require('path');
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
 }
 
 // User APIs
